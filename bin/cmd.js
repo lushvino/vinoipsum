a1ef22b4a02220421fdfc513fa9718e4c5b998d5
Mark Lussier <mlussier@gmail.com>
2013-07-11 11:40:28 -0700

    added ability to send ipsum to the clipboard

--- bin/cmd.js
+++ bin/cmd.js
@@ -2,7 +2,7 @@
 
 var optimist   = require('optimist');
 var generator  = require('./../index.js');
-
+var clipboard  = require('./../lib/copy.js')
 
 var options    = {};
 var arguments  = optimist.argv;
@@ -29,7 +29,7 @@ console.log(loremIpsum);
 
 // Copy the lorem ipsum text to the clipboard.
 if (options.copy) {
-  clipper(loremIpsum, function(err) { err ? process.exit(1) : process.exit() });
+  clipboard(loremIpsum, function(err) { err ? process.exit(1) : process.exit() });
 } else {
   process.exit(); // Successful exit.
 }
