bf53c17c8b91f473c2d9ef6bef5328118dfc4ef3
Mark Lussier <mlussier@gmail.com>
2013-07-12 11:19:17 -0700

    cleanup of cli command

--- bin/cmd.js
+++ bin/cmd.js
@@ -2,11 +2,11 @@
 
 var optimist   = require('optimist');
 var generator  = require('./../index.js');
-var clipboard  = require('./../lib/copy.js')
+var clipboard  = require('./../lib/copy.js');
 
 var options    = {};
-var arguments  = optimist.argv;
-var vinoipsum = '';
+var args       = optimist.argv;
+var vinoipsum  = '';
 
 var sentanceMin  = options.sentanceMinimum || 4;
 
@@ -18,12 +18,12 @@ var format       = options.count || 'text';
 
 
 // Generate the lorem ipsum text and print it out.
-vinoipsum = generator(arguments);
+vinoipsum = generator(args);
 
 console.log(vinoipsum);
 
 // Copy the lorem ipsum text to the clipboard.
-if (arguments.copy) {
+if (args.copy) {
   clipboard(vinoipsum, function(err) { err ? process.exit(1) : process.exit() });
 } else {
   process.exit(); // Successful exit.
