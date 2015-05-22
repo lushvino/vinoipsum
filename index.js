b15b62996e05cf3d2e430f6b35e35be948423fad
Mark Lussier <mlussier@gmail.com>
2015-05-22 12:26:24 -0700

    Update packages, reformat code

--- index.js
+++ index.js
@@ -3,37 +3,37 @@ var dict = require('./data/dictionary');
 
 var generator = function(args) {
 
-    var count         = args.count || 1;
-    var convert_utf8  = args.utf8 || true;
-    var format        = 'plain';
-    var unit          = 'paragraph';
+  var count         = args.count || 1;
+  var convert_utf8  = args.utf8 || true;
+  var format        = 'plain';
+  var unit          = 'paragraph';
 
-    if ( args.s || args.sentence) {
-        unit = 'sentence';
-    }
+  if ( args.s || args.sentence) {
+    unit = 'sentence';
+  }
 
-    if ( args.w || args.word) {
-        unit = 'word';
-    }
+  if ( args.w || args.word) {
+    unit = 'word';
+  }
 
-    if ( args.html) {
-        format = 'html';
-    }
+  if ( args.html) {
+    format = 'html';
+  }
 
-    if ( args.json) {
-        format = 'json';
-    }
+  if ( args.json) {
+    format = 'json';
+  }
 
-    var ipsum = blindipsum( {
-        count: count,
-        utf8: convert_utf8,
-        format: format,
-        unit: unit
-    });
+  var ipsum = blindipsum({
+    count: count,
+    utf8: convert_utf8,
+    format: format,
+    unit: unit
+  });
 
-    ipsum.setDictionary(dict);
+  ipsum.setDictionary(dict);
 
-    return ipsum.generate();
+  return ipsum.generate();
 };
 
 module.exports = generator;
